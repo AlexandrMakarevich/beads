a49e581c220939f887933e21a8a726db03c6f0b7
AlexandrMakarevich <alexandr.makarevich88@gmail.com>
2018-03-27 23:12:29 +0300

    created new module beads_vaadin added all needed dependencies in pom

---
+++ beads_vaadin/src/main/java/com/beads/vaadin/MainUI.java
@@ -0,0 +1,12 @@
+package com.beads.vaadin;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.UI;
+
+public class MainUI extends UI {
+
+  @Override
+  protected void init(VaadinRequest request) {
+
+  }
+}
